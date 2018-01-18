244be27c60aa57210624ab1468db40ee3a416242
Ross Kiser <kiserr10@gmail.com>
2018-01-18 11:53:49 -0700

    column now draggabl

--- src/Deck.js
+++ src/Deck.js
@@ -4,15 +4,15 @@ import { View, Text, Animated, PanResponder } from 'react-native';
 class Deck extends Component {
 	constructor(props){
 		super(props);
-
+		const position = new Animated.ValueXY();
 		const panResponder = PanResponder.create({
 			onStartShouldSetPanResponder: () => true,
 			onPanResponderMove: (event, gesture) => {
-				console.log(gesture);
+				position.setValue({ x: gesture.dx, y: gesture.dy });
 			},
 			onPanResponderRelease: () => {}
 		});
-		this.state = { panResponder };
+		this.state = { panResponder, position };
 	}
 	renderCards(){
 		return this.props.data.map(item => {
@@ -22,9 +22,12 @@ class Deck extends Component {
 
 	render(){
 		return (
-			<View>
+			<Animated.View
+				style={this.state.position.getLayout()}
+				{...this.state.panResponder.panHandlers} 
+			>
 				{this.renderCards()}
-			</View>
+			</Animated.View>
 		);
 	}
 }
