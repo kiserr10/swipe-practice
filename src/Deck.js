5edfb2e02defae095b0b63d38537191d9d2f5d04
Ross Kiser <kiserr10@gmail.com>
2018-01-20 08:30:52 -0700

    updated deck component

--- src/Deck.js
+++ src/Deck.js
@@ -12,7 +12,13 @@ const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
 const SWIPE_OUT_DURATION = 250;
 
 class Deck extends Component {
-	constructor(props){
+
+	static defaultProps = {
+		onSwipeRight: () => {},
+		onSwipeLeft: () => {},
+	}
+
+	constructor(props) {
 		super(props);
 		const position = new Animated.ValueXY();
 		const panResponder = PanResponder.create({
@@ -21,9 +27,9 @@ class Deck extends Component {
 				position.setValue({ x: gesture.dx, y: gesture.dy });
 			},
 			onPanResponderRelease: (event, gesture) => {
-				if (gesture.dx > SWIPE_THRESHOLD){
+				if (gesture.dx > SWIPE_THRESHOLD) {
 					this.forceSwipe('right');
-				} else if (gesture.dx < -SWIPE_THRESHOLD){
+				} else if (gesture.dx < -SWIPE_THRESHOLD) {
 					this.forceSwipe('left');
 				} else {
 					this.resetPosition();
@@ -33,7 +39,7 @@ class Deck extends Component {
 		this.state = { panResponder, position };
 	}
 
-	forceSwipe(direction){
+	forceSwipe(direction) {
 		const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
 		Animated.timing(this.state.position, {
 			toValue: { x: x * 2, y: 0 },
@@ -41,28 +47,35 @@ class Deck extends Component {
 		}).start();
 	}
 
-	resetPosition(){
+	onSwipeComplete(direction) {
+		const { onSwipeLeft, onSwipeRight, data } = this.props;
+		const item = data[this.state.index];
+
+		direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item);
+	}
+
+	resetPosition() {
 		Animated.spring(this.state.position, {
 			toValue: { x: 0, y: 0 }
 		}).start();
 	}
 
-	getCardStyle(){
+	getCardStyle() {
 		const { position } = this.state;
 		const rotate = position.x.interpolate({
 			inputRange: [-SCREEN_WIDTH * 2, 0, SCREEN_WIDTH * 2],
 			outputRange: ['-120deg', '0deg', '120deg']
 		});
-		return{
+		return {
 			...position.getLayout(),
 			transform: [{ rotate }]
 		};
 	}
 
-	renderCards(){
+	renderCards() {
 		return this.props.data.map((item, index) => {
-			if(index === 0){
-				return(
+			if (index === 0) {
+				return (
 					<Animated.View
 						key={item.id}
 						style={this.getCardStyle()}
@@ -76,7 +89,7 @@ class Deck extends Component {
 		});
 	}
 
-	render(){
+	render() {
 		return (
 			<View>
 				{this.renderCards()}
