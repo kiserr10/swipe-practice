23b7f27855de81111fd7ae25ad543ccf10d28e39
Ross Kiser <kiserr10@gmail.com>
2018-01-23 18:52:16 -0700

    swipe creation working and looking googw

--- src/Deck.js
+++ src/Deck.js
@@ -4,7 +4,9 @@ import {
 	Text,
 	Animated,
 	PanResponder,
-	Dimensions
+	Dimensions,
+	LayoutAnimation,
+	UIManager
 } from 'react-native';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
@@ -36,6 +38,17 @@ class Deck extends Component {
 			}
 		});
 		this.state = { panResponder, position, index: 0 };
+	}
+
+	componentWillReceiveProps(nextProps){
+		if(nextProps.data !== this.props.data){
+			this.setState({ index: 0 });
+		}
+	}
+
+	componentWillUpdate(){
+		UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);
+		LayoutAnimation.spring();
 	}
 
 	forceSwipe(direction) {
@@ -74,21 +87,32 @@ class Deck extends Component {
 	}
 
 	renderCards() {
+		if(this.state.index >= this.props.data.length){
+			return this.props.renderNoMoreCards();
+		}
+
 		return this.props.data.map((item, i) => {
 			if (i < this.state.index) { return null; }
 			if (i === this.state.index) {
 				return (
 					<Animated.View
 						key={item.id}
-						style={this.getCardStyle()}
+						style={[this.getCardStyle(), styles.cardStyle]}
 						{...this.state.panResponder.panHandlers}
 					>
 						{this.props.renderCard(item)}
 					</Animated.View>
 				);
 			}
-			return this.props.renderCard(item);
-		});
+			return(
+				<Animated.View
+					key={item.id}
+					style={[styles.cardStyle, { top: 10 * (i - this.state.index) }]}
+				>
+					{this.props.renderCard(item)}
+				</Animated.View>
+			);
+		}).reverse();
 	}
 
 	render() {
@@ -97,6 +121,13 @@ class Deck extends Component {
 				{this.renderCards()}
 			</View>
 		);
+	}
+}
+
+const styles = {
+	cardStyle: {
+		position: 'absolute',
+		width: SCREEN_WIDTH
 	}
 }
 
